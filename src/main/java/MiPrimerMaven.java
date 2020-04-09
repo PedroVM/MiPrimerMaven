141600273b189c1e27da97efeecc68db4ef11578
PedroVM <pedro.vizcaino@alumnos.ui1.es>
2020-04-09 20:04:53 +0200

    Mi siguiente commit 9-4-2020 -1

--- src/main/java/MiPrimerMaven.java
+++ src/main/java/MiPrimerMaven.java
@@ -3,7 +3,7 @@ public class MiPrimerMaven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Mi primer maven 9-4-2020 ");
+System.out.println("Mi primer maven 9-4-2020_2 ");
 	}
 
 }
